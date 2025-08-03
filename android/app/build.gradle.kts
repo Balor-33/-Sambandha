b5212bceed69eeb8fbd57c727c66b878c5c6e265
Balor_33 <Sahilpokharel33@gmail.com>
2025-08-03 21:01:33 +0545

    release

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -10,17 +10,17 @@ android {
     namespace = "com.example.sambandha"
     compileSdk = 35  // Updated to 35 as required by plugins
     ndkVersion = "27.0.12077973"
-
+    
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
         isCoreLibraryDesugaringEnabled = true
     }
-
+    
     kotlinOptions {
         jvmTarget = "1.8"
     }
-
+    
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.sambandha"
@@ -31,12 +31,12 @@ android {
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
-
+    
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
+            // Set custom APK name
+            setProperty("archivesBaseName", "Sambandha-dating-app-v${defaultConfig.versionName}")
         }
     }
 }
